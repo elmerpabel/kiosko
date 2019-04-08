326ad7fc944e9427cbf2d7c6a96c1626a58c4985
elmerpabel <48300603+elmerpabel@users.noreply.github.com>
2019-04-08 00:29:57 -0500

    Update email

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "ephp.celular@gmail.com"
+	        email: "elmerpabel@hotmail.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
